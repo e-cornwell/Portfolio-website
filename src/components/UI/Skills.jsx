8364edbf77fe3b5c09aced63966ce51a64f9021d
Ethan Cornwell <ethan.aubrey22@gmail.com>
2023-04-19 21:19:27 -0600

    added css grid with skills images

--- src/components/UI/Skills.jsx
+++ src/components/UI/Skills.jsx
@@ -10,27 +10,31 @@ const Skills = () => {
                 </h2>
                 <div class="container grid grid-cols-3 gap-2 mx-auto">
                     <div class="w-full rounded">
-                        <img src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=989&q=80"
+                        <img src="https://i0.wp.com/blogs.embarcadero.com/wp-content/uploads/2020/08/JavaScript-logo.png?ssl=1"
                             alt=""/>
                     </div>
                     <div class="w-full rounded">
-                        <img src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=989&q=80"
+                        <img src="http://johnwkirch.com/img/css.png"
                             alt=""/>
                     </div>
                     <div class="w-full rounded">
-                        <img src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=989&q=80"
+                        <img src="http://johnwkirch.com/img/html.svg"
                             alt=""/>
                     </div>
                     <div class="w-full rounded">
-                        <img src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=989&q=80"
+                        <img src="http://johnwkirch.com/img/node.svg"
                             alt=""/>
                     </div>
                     <div class="w-full rounded">
-                        <img src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=989&q=80"
+                        <img src="image.png"
                             alt=""/>
                     </div>
                     <div class="w-full rounded">
-                        <img src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=989&q=80"
+                        <img src="http://johnwkirch.com/img/sql.png"
+                            alt=""/>
+                    </div>
+                    <div class="w-full rounded">
+                        <img src="http://johnwkirch.com/img/git.svg"
                             alt=""/>
                     </div>
                 </div>
