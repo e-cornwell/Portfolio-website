98176cd62b5179249965a85a2aecd12e9624167f
Ethan Cornwell <ethan.aubrey22@gmail.com>
2023-05-03 14:33:59 -0600

    finished footer and implimenting mobile mode

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,32 +1,122 @@
 import React from "react";
 
 const Footer = () => {
-    return <footer className="bg-[#12141e] pt-12">
 
-    {/* __________ FOOTER TOP __________ */}
+  const year = new Date().getFullYear();  
 
-    <div className="container">
+  return (
+    <footer className="bg-[#12141e] pt-12">
+      {/* __________ FOOTER TOP __________ */}
+
+      <div className="container">
         <div className="sm:flex items-center justify-between md:gap-8">
-            <div className="w-full sm:w-1/2">
-                <h2 className="text-[26px] leading-10 text-white font-[600] mb-5 md:text-[2rem]">
-                    Footer
-                </h2>
-                <a href="#contact">
-                <button
-                  className="bg-primaryColor text-white font-[500] flex items-center gap-2
+          <div className="w-full sm:w-1/2">
+            <h2 className="text-[26px] leading-10 text-white font-[600] mb-5 md:text-[2rem]">
+              Footer
+            </h2>
+            <a href="#contact">
+              <button
+                className="bg-primaryColor text-white font-[500] flex items-center gap-2
                 hover:bg-smallTextColor ease-in duration-300 py-2 px-4 rounded-[8px]"
+              >
+                <i class="ri-mail-line"></i>
+                Contact
+              </button>
+            </a>
+          </div>
+
+          <div className="w-full sm:w-1/2">
+            <p className="text-gray-300 leading-7 mt-4 sm:mt-0">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia
+              praesentium atque assumenda maiores veniam? Molestias at quae aut
+              officia veritatis!
+            </p>
+
+            <div className="flex items-center gap-4 flex-wrap md:gap-8 mt-10">
+              <span className="text-gray-300 font-[600] text-[15px]">
+                Professional Links:
+              </span>
+
+              <span className="w-[35px] h-[35px] bg-[#2b2d33] p-1 rounded-[50px] cursor-pointer text-center">
+                <a
+                  href="https://www.linkedin.com/in/ethan-cornwell/"
+                  className="text-gray-300 text-[18px] font-[500]"
                 >
-                  <i class="ri-mail-line"></i>
-                  Contact
-                </button>
-              </a>
+                  <i class="ri-linkedin-fill"></i>
+                </a>
+              </span>
+              <span className="w-[35px] h-[35px] bg-[#2b2d33] p-1 rounded-[50px] cursor-pointer text-center">
+                <a
+                  href="https://github.com/e-cornwell"
+                  className="text-gray-300 text-[18px] font-[500]"
+                >
+                  <i class="ri-github-fill"></i>
+                </a>
+              </span>
+              <span className="w-[35px] h-[35px] bg-[#2b2d33] p-1 rounded-[50px] cursor-pointer text-center">
+                <a
+                  href="https://twitter.com/EthanCornwell11"
+                  className="text-gray-300 text-[18px] font-[500]"
+                >
+                  <i class="ri-twitter-fill"></i>
+                </a>
+              </span>
             </div>
+          </div>
         </div>
-    </div>
 
-    {/* __________ FOOTER BOTTOM __________ */}
+        <div>
+          <ul className="flex items-center justify-center gap-10 mt-10">
+            <li>
+              <a className="text-gray-400 font-[600]" href="#about">
+                About
+              </a>
+            </li>
+            <li>
+              <a className="text-gray-400 font-[600]" href="#projects">
+                Projects
+              </a>
+            </li>
+            <li>
+              <a className="text-gray-400 font-[600]" href="#skills">
+                Skills
+              </a>
+            </li>
+            <li>
+              <a className="text-gray-400 font-[600]" href="#contact">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      {/* __________FOOTER TOP END ____________ */}
 
+      {/* __________ FOOTER BOTTOM START __________ */}
+      <div className="bg-[#1b1e29] py-5 mt-14">
+        <div className="container">
+          <div className="flex items-center justify-center sm:justify-between">
+            <div className="hidden sm:block">
+              <div className="flex items-center gap-[10px]">
+                <span className="w-[35px] h-[35px] rounded-full bg-[#2b2d33] text-white font-[500] text-[18px] flex items-center justify-center">
+                  QB
+                </span>
+
+                <div className="leading-[20px]">
+                    <h2 className="text-gray-200 font-[500] text-[18px]">Quickbeam Studios</h2>
+                </div>
+              </div>
+            </div>
+            <div>
+                <p className="text-gray-400 text-[14px]">Copyright {year} Quickbeam Studios - All rights reserved.</p>
+                <p className="indent-{10px} text-gray-400 text-[14px]">Logo designed and created by Maya Cornwell - All rights reserved.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* __________ FOOTER BOTTOM END __________ */}
     </footer>
-}
+  );
+};
 
 export default Footer;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,46 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 
 const Header = () => {
+  const headerRef = useRef(null);
+  const menuRef = useRef(null);
+
+  const stickyHeaderFunc = () => {
+    window.addEventListener("scroll", () => {
+      if (
+        document.body.scrollTop > 80 ||
+        document.documentElement.scrollTop > 80
+      ) {
+        headerRef.current.classList.add("sticky__header");
+      } else {
+        headerRef.current.classList.remove("sticky__header");
+      }
+    });
+  };
+
+  useEffect(() => {
+    stickyHeaderFunc();
+    return window.removeEventListener("scroll", stickyHeaderFunc);
+  }, []);
+
+  const handleClick = (ev) => {
+    ev.preventDefault();
+
+    const targetAttr = ev.target.getAttribute("href");
+    const location = document.querySelector(targetAttr).offsetTop;
+
+    window.scrollTo({
+      top: location - 80,
+      left: 0,
+    });
+  };
+
+  const toggleMenu = () => menuRef.current.classList.toggle('show__menu')
+
   return (
-    <header className="w-full h-[80px] leading-[80px] flex-items-center">
+    <header
+      ref={headerRef}
+      className="w-full h-[80px] leading-[80px] flex-items-center"
+    >
       <div className="container">
         <div className="flex items-center justify-between">
           {/* == LOGO == */}
@@ -18,27 +56,48 @@ const Header = () => {
               <h2 className="text-xl text-smallTextColor font-[700]">
                 Quickbeam Studios
               </h2>
-              <p className="text-smallTextColor text-[16px] font-[500]">
-                Personal Site
-              </p>
             </div>
           </div>
 
           {/* == LOGO END == */}
           {/* == MENU START == */}
-          <div className="menu">
+          <div className="menu" ref={menuRef} onClick={toggleMenu}>
             <ul className="flex items-center gap-10">
               <li>
-                <a className="text-smallTextColor font-[600]" href="#projects">Projects</a>
+                <a
+                  onClick={handleClick}
+                  className="text-smallTextColor font-[600]"
+                  href="#about"
+                >
+                  About
+                </a>
               </li>
               <li>
-                <a className="text-smallTextColor font-[600]" href="#about">About</a>
+                <a
+                  onClick={handleClick}
+                  className="text-smallTextColor font-[600]"
+                  href="#projects"
+                >
+                  Projects
+                </a>
               </li>
               <li>
-                <a className="text-smallTextColor font-[600]" href="#skills">Skills</a>
+                <a
+                  onClick={handleClick}
+                  className="text-smallTextColor font-[600]"
+                  href="#skills"
+                >
+                  Skills
+                </a>
               </li>
               <li>
-                <a className="text-smallTextColor font-[600]" href="contact">Contact</a>
+                <a
+                  onClick={handleClick}
+                  className="text-smallTextColor font-[600]"
+                  href="#contact"
+                >
+                  Contact
+                </a>
               </li>
             </ul>
           </div>
@@ -46,13 +105,15 @@ const Header = () => {
 
           {/* == MENU RIGHT == */}
           <div className="flex item-center gap-4">
-            <button className="flex items-center gap-2 text-smallTextColor font-[600] 
+            <button
+              className="flex items-center gap-2 text-smallTextColor font-[600] 
                 border border-solid border-smallTextColor py-2 px-4 rounded-[8px] max-h-[40px] hover:bg-smallTextColor
-                hover:text-white hover:font-[500] ease-in duration-300">
-                <i class="ri-send-plane-line"></i> Let's Talk
+                hover:text-white hover:font-[500] ease-in duration-300"
+            >
+              <i class="ri-send-plane-line"></i> Let's Talk
             </button>
-            <span className="text-2xl text-smallTextColor md:hidden cursor-pointer">
-                <i class="ri-menu-line"></i>
+            <span onClick={toggleMenu} className="text-2xl text-smallTextColor md:hidden cursor-pointer">
+              <i class="ri-menu-line"></i>
             </span>
           </div>
           {/* == MENU END == */}
